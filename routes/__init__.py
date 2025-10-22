9baaa8d03c4dc12a9622a3d717a05d8c0d2b077f
Juraci Moura <juniornino084@gmail.com>
2025-10-22 20:30:24 -0300

    remoção de rota duplicada

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,8 @@
 from .present_routes import present_bp
 from .payment_routes import payment_bp
+from .webhook import webhook_bp
 
 def register_routes(app):
     app.register_blueprint(present_bp)
     app.register_blueprint(payment_bp)
+    app.register_blueprint(webhook_bp)
